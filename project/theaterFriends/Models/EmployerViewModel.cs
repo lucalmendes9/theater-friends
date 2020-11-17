31a2f590523860cc16e168f26398157244420fbe
Rafael Câmara Araújo <cfael.21@gmail.com>
2020-11-17 13:56:25 -0300

    ViewModels e comeco das DAO

---
+++ project/theaterFriends/Models/EmployerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace theaterFriends.Models
+{
+    public class EmployerViewModel : PadraoViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Hired_At { get; set; }
+    }
+}
---
+++ project/theaterFriends/Models/ExhibitionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace theaterFriends.Models
+{
+    public class ExhibitionViewModel : PadraoViewModel
+    {
+        public int Room_id { get; set; }
+        public int Movie_id { get; set; }
+    }
+}
