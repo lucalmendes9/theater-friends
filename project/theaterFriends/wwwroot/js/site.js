502dbd452f0d6c3b821d71af794746bad8d1e046
Lucas Lima <lucalmendes9@gmail.com>
2020-11-24 20:26:26 -0300

    add delete in production

--- project/theaterFriends/wwwroot/js/site.js
+++ project/theaterFriends/wwwroot/js/site.js
@@ -1,2 +1,15 @@
-﻿
-
+﻿function apagar(id, controller) {
+    swal({
+        title: "Você realmente deseja Excluir?",
+        text: "Esse registro sera apagado do sistema!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonClass: "btn-danger",
+        confirmButtonText: "Sim",
+        cancelButtonText: "Não",
+        closeOnConfirm: false
+    },
+        function () {
+            location.href = '/' + controller + '/Delete?id=' + id;
+        });
+}
